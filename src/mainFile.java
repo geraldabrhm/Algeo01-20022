9a589b78932410f7a084280d5f31d06f9cab8740
TheOne28 <vincentpatmadja.gmail.com>
2021-09-29 17:05:33 +0700

    mainFile: add basic input output

---
+++ src/mainFile.java
@@ -0,0 +1,129 @@
+import java.text.NumberFormat.Style;
+import java.io.*;
+import java.util.*;
+
+public class mainFile {
+    public static Scanner scan = new Scanner(System.in);
+    
+    public static int CheckInteger(int min, int max, String message){
+        while (true){
+            try{
+                System.out.print(message);
+                int a = scan.nextInt();
+                if((a >= min) && (a <= max)){
+                    return a;
+                }else{
+                    System.out.println("Integer di luar range.");
+                }
+            }
+            catch (InputMismatchException e){
+                scan.next();
+                System.out.println("Input bukan integer.");
+            }
+        }
+    }
+    
+    public static int whatMethod(int numb){
+        int a = -1;
+        switch(numb){
+            case 1:
+                System.out.println("Ada 4 metode penyelesaian yang tersedia, silahkan pilih dengan mengetikkan nomor yang sesuai");
+                System.out.println("Metode Penyelesaian: ");
+                System.out.println("1. Metode Eliminasi Gauss");
+                System.out.println("2. Metode Eliminasi Gauss Jordan");
+                System.out.println("3. Metode Matriks Balikan");
+                System.out.println("4. Kaidah Crammer");
+                a = CheckInteger(1, 4, "Metode Penyelesaian: ");                
+                break;
+            case 2:
+                System.out.println("Ada 2 metode penyelesaian yang tersedia, silahkan pilih dengan mengetikkan nomor yang sesuai");
+                System.out.println("Metode Penyelesaian: ");                
+                System.out.println("1. Metode Reduksi Baris");
+                System.out.println("2. Ekspansi Kofaktor");
+                a = CheckInteger(1, 2, "Metode Penyelesaian: ");                
+                break;
+            case 3:
+                System.out.println("Ada 2 metode penyelesaian yang tersedia, silahkan pilih dengan mengetikkan nomor yang sesuai");
+                System.out.println("Metode Penyelasaian: ");
+                System.out.println("1. Metode Eliminasi Gauss Jordan");
+                System.out.println("2. Matriks Adjoin");
+                a = CheckInteger(1, 2, "Metode Penyelesaian: ");                
+                break;
+        }
+        
+        return a;
+    }
+
+    public static int whatInput(){
+        System.out.println("Ada 2 metode input yang tersedia, silahkan pilih dengan mengetikkan nomor yang sesuai");
+        System.out.println("Metode Input: ");
+        System.out.println("1. Terminal");
+        System.out.println("2. File");
+        
+        int a = CheckInteger(1, 2, "Metode Input: ");
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int getservice;
+        int getmethod;
+        int getinput;
+        
+        System.out.println();
+        System.out.println("--------Welcome to Linear Algebra Solver--------- ");
+        System.out.println();
+        
+        System.out.println("Ditempat ini, kami akan membantumu memecahkan persoalan aljabar linier :)");
+        System.out.println();
+        
+        System.out.println("How? My dear customer, kamu bisa memilih 1 dari servis yang tersedia di bawah ini");
+        System.out.println("Setelah itu, kamu harus melengkapi spesifikasinya, yaitu metode penyelesaian, \n metode input, dan input yang sesuai");
+        System.out.println();
+
+        System.out.println("Ada 6 servis yang tersedia, silahkan pilih dengan mengetikkan nomor yang sesuai");        
+        System.out.println("Servis tersedia: ");
+        System.out.println("1. Memecahkan Sistem Persamaan Linear");
+        System.out.println("2. Mencari Determinan Matriks");
+        System.out.println("3. Menentukan Balikan Matriks");
+        System.out.println("4. Memecahkan Interpolasi Polinom");
+        System.out.println("5. Memecahkan Regresi Linear Berganda");
+        System.out.println("6. Exit Program");
+        
+        System.out.println();
+        getservice = CheckInteger(1, 6, "Servis: ");
+        
+        switch(getservice){
+            case 1:
+                System.out.println("Selamat datang di Sistem Persamaan Linear");
+                getmethod = whatMethod(1);
+                getinput = whatInput();
+                break;
+            case 2:
+                System.out.println("Selamat datang di Determinan Matriks");
+                getmethod = whatMethod(2);
+                getinput = whatInput();
+                break;
+            case 3:
+                System.out.println("Selamat datang di Sistem Persamaan Linear");
+                getmethod = whatMethod(1);
+                getinput = whatInput();
+                break;
+            case 4:
+                System.out.println("Selamat datang di Interpolasi Polinom");
+                getinput = whatInput();
+                getmethod = -1;
+                break;
+            case 5:
+                System.out.println("Selamat datang di Regresi LInear Berganda");
+                getinput = whatInput();
+                getmethod = -1;
+                break;
+            case 6:
+                System.out.println("Terima kasih sudah menggunakan Linear Algebra Solver");
+                System.out.println("Have a nice day :)");
+                System.exit(0);
+        }
+
+        
+    }
+}
